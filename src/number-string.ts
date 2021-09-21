51671a0cd9db8954be948f19b8eeaa0e9dac6aaf
Hiratake <hiratake@hiratake.xyz>
2021-09-21 23:36:12 +0900

    chore: number-string.tsを追加

---
+++ src/number-string.ts
@@ -0,0 +1,15 @@
+export {}
+
+let year: number = 1976
+year = 1997
+// year = false
+console.log(year)
+
+let age: number = 0x2b
+age = 0x17
+console.log(age)
+
+let name: string = 'Hiratake'
+name = 'Nanami'
+// name = 10
+console.log(name)
