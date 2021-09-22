1af354f88245ecbf8c2b059217f02ebdd37f7cf7
Hiratake <hiratake@hiratake.xyz>
2021-09-22 20:08:01 +0900

    chore: array.tsを追加

---
+++ src/array.ts
@@ -0,0 +1,19 @@
+export {}
+
+const numbers: number[] = [1, 2, 3]
+console.log(numbers)
+
+const strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript']
+console.log(strings)
+
+// const numbers2: Array<number> = [1, 2, 3]
+// const strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto']
+
+const nijigenHairetsu: number[][] = [
+  [50, 100],
+  [150, 300],
+]
+console.log(nijigenHairetsu)
+
+const hairetsu: (string | number | boolean)[] = [1, false, 'Japan']
+console.log(hairetsu)
