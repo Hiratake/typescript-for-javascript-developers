a81fb7c85d4c45cbe4012947afc45a979f2b24ae
Hiratake <hiratake@hiratake.xyz>
2021-09-22 21:31:15 +0900

    chore: void.tsを追加

---
+++ src/void.ts
@@ -0,0 +1,12 @@
+export {}
+
+// const returnNothing = (): undefined => {
+//   console.log("I don't return anything")
+//   return undefined
+// }
+
+const returnNothing = (): void => {
+  console.log("I don't return anything")
+}
+
+console.log(returnNothing())
