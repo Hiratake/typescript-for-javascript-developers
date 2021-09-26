15992418381053a5fa65ae020134eee9d80c8688
Hiratake <hiratake@hiratake.xyz>
2021-09-26 16:00:09 +0900

    chore: abstract-classes.tsを追加

---
+++ src/abstract-classes.ts
@@ -0,0 +1,20 @@
+export {}
+
+abstract class Animal {
+  abstract cry(): string
+}
+
+class Lion extends Animal {
+  cry() {
+    return 'roar'
+  }
+}
+
+class Tiger extends Animal {
+  cry() {
+    return ''
+  }
+}
+
+console.log(Lion)
+console.log(Tiger)
