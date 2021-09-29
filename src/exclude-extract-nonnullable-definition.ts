9b1cb4965450feca79aad298cd5c4e6696d07445
Hiratake <hiratake@hiratake.xyz>
2021-09-29 21:29:14 +0900

    chore: exclude-extract-nonnullable-definitionを追加

---
+++ src/exclude-extract-nonnullable-definition.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export {}
+
+type DebugType = () => void
+type SomeTypes = string | number | DebugType
+
+type FunctionType = Exclude<SomeTypes, string | number>
+
+// type MyExclude<T, U> = T extends U ? never : T
+// type MyFunctionType = MyExclude<SomeTypes, string | number>
+type MyExclude =
+  | (string extends string | number ? never : string) // -> never
+  | (number extends string | number ? never : number) // -> never
+  | (DebugType extends string | number ? never : DebugType) // -> DebugType
+type MyFunctionType = MyExclude
+
+type FunctionTypeByExtract = Extract<SomeTypes, DebugType>
+
+type NullableTypes = string | number | null | undefined
+type NonNullableTypes = NonNullable<NullableTypes>
